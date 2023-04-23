9e8c1ad5723a314ecc8432a34155239270d85474
cameronjpr <cameronjpr@gmail.com>
2023-04-23 12:25:04 +0100

    cleanup;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 import { defineConfig } from 'astro/config'
 
-import sitemap from '@astrojs/sitemap'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://cameronjpr.vercel.app/',
-  integrations: [sitemap()],
-  experimental: {
-    assets: true,
-  },
 })
